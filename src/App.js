eff88438b6efe7377832e71e7483d85ddd93693b
boxak <boxak@naver.com>
2022-02-15 05:40:57 +0900

    className으로 CSS 적용

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import React from 'react';
 
 function App() {
   const name = '리액트';
-  const style = {
-    backgroundColor : 'black',
-    color : 'aqua',
-    fontSize : '48px',
-    fontWeight : 'bold',
-    padding : 16
-  };
-
-  return <div style={style}>{name}</div>
+  return <div className='react'>{name}</div>
 }
 
 export default App;
