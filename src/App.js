ff4ee7a92843f6b270c7826890f8761df327a240
boxak <boxak@naver.com>
2022-02-15 05:33:22 +0900

    && 연산자를 사용한 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const name = "뤼액트";
   return (
     <div>
-      {name === '리액트' ? (<h1>리액트입니다.</h1>) : (<h2>리액트가 아닙니다.</h2>)}
+      {name === '리액트' && <h1>리액트입니다.</h1>}
     </div>
   );
 }
