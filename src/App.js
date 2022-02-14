c38fb0528a914594d034e6ffbe1af36c5f1828d1
boxak <boxak@naver.com>
2022-02-15 05:51:02 +0900

    쉬프트+알트+F 로 prettier code format을 실행할 수 있다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
-  const name = '리액트';
-  return <div className='react'>{name}</div>
+  const name = "리액트";
+  return (
+    <div>
+      <div className="react">{name}</div>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다.</p>
+    </div>
+  );
 }
 
 export default App;
