2180c41e3c0bbd7e623c317496f7fa897a656fc1
boxak <boxak@naver.com>
2022-02-25 22:33:43 +0900

    propTypes 를 이용한 변수 타입 강제하기

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent>리액트</MyComponent>
+  return <MyComponent name='react' favoriteNumber={1}>리액트</MyComponent>
 }
 
 export default App;
