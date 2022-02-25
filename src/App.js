c1f4c52fdd866616eedfd9eff1f9ab32c2b56107
boxak <boxak@naver.com>
2022-02-25 22:48:40 +0900

    state로 바뀌는 값을 관리할 수 있다.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-  return <MyComponent name='react' favoriteNumber={1}>리액트</MyComponent>
+  return <Counter />
 }
 
 export default App;
