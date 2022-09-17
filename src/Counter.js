50582c8756820ab984cfe4a697e41bae671d4df9
boxak <boxak@naver.com>
2022-09-17 22:19:24 +0900

    setState 두번째 인자로 콜백 함수를 넣어 비동기 실행이 가능

--- src/Counter.js
+++ src/Counter.js
@@ -13,10 +13,12 @@ class Counter extends Component {
                 <h1>{number}</h1>
                 <h2>바뀌지 않는 값 : {fixedNumber}</h2>
                 <button onClick={()=>{
-                    this.setState(prevState => {
-                        return {
-                            number : prevState.number + 1
-                        }
+                    this.setState({
+                        number : number + 1
+                    },
+                    () => {
+                        console.log('방금 setState가 호출되었습니다');
+                        console.log(this.state);
                     });}}>+1</button>
             </div>
         );
