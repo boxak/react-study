1058777207b20a594b1da8c95f98d62281a114e1
boxak <boxak@naver.com>
2022-09-18 23:39:15 +0900

    form 으로 username, message를 한번에 세팅

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,14 +1,27 @@
 import React, { Component, useState } from 'react';
 
 const EventPractice = () => {
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
+    const [form, setForm] = useState({
+        username : '',
+        message : ''
+    });
+
+    const {username, message} = form;
+
+    const onChange = e => {
+        const nextForm = {
+            ...form,
+            [e.target.name] : e.target.value
+        };
+        setForm(nextForm);
+    }
+
     const onClick = () => {
         alert(username + " : " + message);
-        setUsername('');
-        setMessage('');
+        setForm({
+            username : '',
+            message : ''
+        });
     };
 
     const onKeyPress = e => {
@@ -25,14 +38,14 @@ const EventPractice = () => {
           name="username"
           placeholder="사용자명"
           value={username}
-          onChange={onChangeUsername}
+          onChange={onChange}
         ></input>
         <input
           type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={message}
-          onChange={onChangeMessage}
+          onChange={onChange}
           onKeyPress={onKeyPress}
         ></input>
         <button
