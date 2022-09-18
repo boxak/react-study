6031c65bdc979b491a314d93ab69406c9757cb6f
boxak <boxak@naver.com>
2022-09-18 23:17:08 +0900

    여러 input event handling

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
   state = {
     message: '',
+    username:''
   };
 
   constructor(props) {
@@ -10,15 +11,18 @@ class EventPractice extends Component {
   }
 
   handleChange = (e) => {
+    // 객체 안에서 key를 []로 감싸면 그 안에 넣은
+    // 레퍼런스가 가리키는 실제 값이 key로 사용.
     this.setState({
-        message : e.target.value
+        [e.target.name]:e.target.value
     });
   }
 
   handleClick = () => {
-    alert(this.state.message);
+    alert(this.state.username+" : "+this.state.message);
     this.setState({
-        message : ''
+        message : '',
+        username:''
     });
   }
 
@@ -28,8 +32,15 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        ></input>
+        <input
+          type="text"
           name="message"
-          placeholder="아무거나 입력해 보세요."
+          placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
         ></input>
