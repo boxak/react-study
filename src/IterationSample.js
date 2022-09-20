8f2611182e3f28ed2d1ef0e79b8f9544b3ef84d4
boxak <boxak@naver.com>
2022-09-20 22:32:41 +0900

    concat 을 통해 배열에 값이나 객체 추가

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
 
 const IterationSample = () => {
 
@@ -13,10 +14,31 @@ const IterationSample = () => {
 
     const [nextId, setNextId] = useState(5);
 
+    const onChange = e => setInputText(e.target.value);
+
+    const input = useRef("");
+
+    const onClick = () => {
+        const nextNames = names.concat({
+           id : nextId,
+           text : inputText 
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+ 
+        input.current.focus();
+    }
+
     const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
 
     return (
-        <ul>{namesList}</ul>
+        <Fragment>
+            <input value={inputText} onChange={onChange} 
+            ref={input}/>
+            <button onClick={onClick}>추가</button>
+            <ul>{namesList}</ul>
+        </Fragment>
     );
 };
 
