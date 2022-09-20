ebb1fa1ae66fe9d323aa1ced0cface35eba06ad3
boxak <boxak@naver.com>
2022-09-20 23:17:05 +0900

    class 컴포넌트에서의 Life Cycle

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,30 @@ import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import React, { Component } from 'react';
 import IterationSample from './IterationSample';
+import LifeCycleSamle from './LifeCycleSamle';
+
+function getRandomColor() {
+  return '#'+Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
 
+  state = {
+    color : '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color : getRandomColor()
+    });
+  }
+
   render() {
     return(
-      <IterationSample/>
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSamle color = {this.state.color}/>
+      </div>
     );
   };
 }
